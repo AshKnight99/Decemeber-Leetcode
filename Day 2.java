bd5308de20fa91c80a55101e9001594a9eaed009
Ashknight99 <rachanaraut1999@gmail.com>
2020-12-02 18:28:25 +0530

    Day 2 commit

---
+++ Day 2.java
@@ -0,0 +1,95 @@
+/*
+Linked List Random Node
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+*/
+
+// By Reservoir Sampling Algorithm (O(n) -> Time Complexity , O(1) -> Space Complexity)
+class Solution {
+    private ListNode head;
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int scope = 1, chosenValue = 0;
+        ListNode curr = this.head;
+        while (curr != null) {
+            // decide whether to include the element in reservoir
+            if (Math.random() < 1.0 / scope)
+                chosenValue = curr.val;
+            // move on to the next node
+            scope += 1;
+            curr = curr.next;
+        }
+        return chosenValue;
+    }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*
+O (n) Space complexity with array list
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+   
+    List <Integer> list ;
+    public Solution(ListNode head) {
+        list = new ArrayList<>();
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        return list.get(new Random().nextInt(list.size()));
+        
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+*/
+
